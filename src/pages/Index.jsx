2cb5ff1dd90b61d4bab5d6bc782cf08a947955a1
Chava <schavarria13@alumnos.uaq.mx>
2023-03-24 14:20:45 -0600

    Routing, Layout, Navigate between components and loader

---
+++ src/pages/Index.jsx
@@ -0,0 +1,18 @@
+export function loader(){
+
+  return "Desde Loader";
+}
+
+function Index() {
+
+
+
+  return (
+    <>
+      <h1 className="font-black text-4xl text-blue-900">Clientes</h1>
+      <p className="mt-3">Administra tus Clientes</p>
+    </>
+  )
+}
+
+export default Index
